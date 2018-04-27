48f7f07fcd66294ab4421daa4b180c96a3ac573d
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-04-27 14:14:18 +0200

    feat(setup.py) file created

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sitemap_generator',
+      version='1.4',
+      description='Sitemap Generator',
+      license='BSD',
+      author='Google Inc & Dimitri DO BAIRRO',
+      author_email='dimitri.dobairro@dimsolution.com',
+      url='https://github.com/rimiti/sitemap-generator',
+     )
